c0b203bc5c1e2485bd477e882e9091847a4fe1e0
Jeremiah Teague <jeremiahteague@gmail.com>
2022-09-28 15:25:56 -0600

    API initial commit

---
+++ src/api/WebAppTest/WebAppTest/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetRandomInt")]
+        public int GetRandomInt()
+        {
+            return new Random().Next(0, 1000);
+        }
+    }
+}
