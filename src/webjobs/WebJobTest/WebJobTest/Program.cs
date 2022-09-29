6c7df4a3164ff6babf336b35491fb03bcc9d37cf
Jeremiah Teague <jeremiahteague@gmail.com>
2022-09-29 11:26:16 -0600

    Added webjob

---
+++ src/webjobs/WebJobTest/WebJobTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebJobsSDKSample
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var builder = new HostBuilder();
+            
+            builder.UseEnvironment(EnvironmentName.Development);
+
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorageQueues();
+            });
+
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
